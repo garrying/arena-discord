50c8ef92a79615a67323054f031474362e2b145c
Garry Ing <garry.ing@gmail.com>
2025-10-19 13:23:06 -0400

    update

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-import arenaFeed from "arena-feed";
 import { Client, Events, GatewayIntentBits } from "discord.js";
-import added from "./lib/arenaAdded.js";
-import commented from "./lib/arenaCommented.js";
-import followed from "./lib/arenaFollowed.js";
+import processArenaFeed from "./lib/arenaEmbedFeed.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const arenaPersonalFeed = await arenaFeed(process.env.ARENA_TOKEN);
 
-client.once(Events.ClientReady, (readyClient) => {
+client.once(Events.ClientReady, async (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-  const stories = arenaPersonalFeed.items;
 
   const channelId = process.env.CHANNEL_ID;
   const channel = client.channels.cache.get(channelId);
 
   if (channel) {
-    const tenMinutesAgo = Date.now() - 10 * 60 * 1000;
-
-    stories.forEach((story) => {
-      const storyTime = new Date(story.created_at).getTime();
-      if (storyTime >= tenMinutesAgo) {
-        if (story.action == "added") {
-          const embed = added(story);
-          channel.send({ embeds: [embed] });
-        } else if (story.action == "followed") {
-          const embed = followed(story);
-          channel.send({ embeds: [embed] });
-        } else if (story.action == "commented on") {
-          const embed = commented(story);
-          channel.send({ embeds: [embed] });
-        }
-      }
-    });
+    await processArenaFeed(channel);
   } else {
     console.log("Channel not found");
   }
