455bfb99f682f2d7589abfa7822619d109d76d69
Garry Ing <garry.ing@gmail.com>
2026-03-02 21:25:49 -0500

    bump packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 import js from "@eslint/js";
-import importPlugin from "eslint-plugin-import";
 import globals from "globals";
 
 export default [
   js.configs.recommended,
-  importPlugin.flatConfigs.recommended,
   {
     files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
